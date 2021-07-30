a5b69b3a24bd5e6974931fe81a6030d560d76ab5
hilmanski <hilman@hilman.com>
2021-07-30 10:01:12 +0800

    experiment with sidebar

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,23 +2,23 @@
 //Dark Mode
 //=====================
 //check localstorage
-if(localStorage.getItem('preferredTheme') == 'dark')
-    setDarkMode()
+// if(localStorage.getItem('preferredTheme') == 'dark')
+//     setDarkMode()
 
-document.getElementById('darkModeBtn').addEventListener("click", function(){
-    setDarkMode()
-})
+// document.getElementById('darkModeBtn').addEventListener("click", function(){
+//     setDarkMode()
+// })
 
-function setDarkMode() {
-    let emoticon = ''
-    let isDark = document.body.classList.toggle("darkmode");
+// function setDarkMode() {
+//     let emoticon = ''
+//     let isDark = document.body.classList.toggle("darkmode");
 
-    if(isDark) {
-        emoticon = '🌝'
-        localStorage.removeItem('preferredTheme');
-    } else {
-        emoticon =  '🌚'
-        localStorage.setItem('preferredTheme', 'dark');
-    }
-    document.getElementById('darkModeBtn').innerHTML = emoticon
-}
+//     if(isDark) {
+//         emoticon = '🌝'
+//         localStorage.removeItem('preferredTheme');
+//     } else {
+//         emoticon =  '🌚'
+//         localStorage.setItem('preferredTheme', 'dark');
+//     }
+//     document.getElementById('darkModeBtn').innerHTML = emoticon
+// }
