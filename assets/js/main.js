5d07bd9ef0314e7de1e0271c69d4c6eda9acaa8b
hilmanski <hilman@hilman.com>
2021-07-31 10:24:04 +0800

    show only 20 cateogry first

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,29 @@
+// Get the modal
+var modal = document.getElementById("modalwrapper");
+
+// Get the button that opens the modal
+var btn = document.getElementById("moreTagsBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 //=====================
 //Dark Mode
 //=====================
