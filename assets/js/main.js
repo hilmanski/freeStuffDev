e995a2b74c30a67bf5b65b8257eb6ef84a624b5e
Rahmat Agung Julians <rahmatagungj@Gmail.com>
2021-09-07 21:50:21 +0700

    applying dark mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,44 +7,43 @@ var btn = document.getElementById("moreTagsBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
+// When the user clicks the button, open the modal
+btn.onclick = function () {
   modal.style.display = "block";
-}
+};
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
-}
+};
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+};
+
+// =====================
+// Dark Mode
+// =====================
+function setDarkMode() {
+  let status = "";
+  const isDark = document.body.classList.toggle("darkmode");
+
+  if (isDark) {
+    status = "🌝 Light";
+    localStorage.setItem("preferredTheme", "dark");
+  } else {
+    status = "🌚 Dark";
+    localStorage.removeItem("preferredTheme");
+  }
+  document.getElementById("darkModeBtn").innerHTML = status;
 }
 
-//=====================
-//Dark Mode
-//=====================
-//check localstorage
-// if(localStorage.getItem('preferredTheme') == 'dark')
-//     setDarkMode()
+// check localstorage
+if (localStorage.getItem("preferredTheme") == "dark") setDarkMode();
 
-// document.getElementById('darkModeBtn').addEventListener("click", function(){
-//     setDarkMode()
-// })
-
-// function setDarkMode() {
-//     let emoticon = ''
-//     let isDark = document.body.classList.toggle("darkmode");
-
-//     if(isDark) {
-//         emoticon = '🌝'
-//         localStorage.removeItem('preferredTheme');
-//     } else {
-//         emoticon =  '🌚'
-//         localStorage.setItem('preferredTheme', 'dark');
-//     }
-//     document.getElementById('darkModeBtn').innerHTML = emoticon
-// }
+document.getElementById("darkModeBtn").addEventListener("click", function () {
+  setDarkMode();
+});
