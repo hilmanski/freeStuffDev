adc71616d4402073858e1e3a39c622d65ada53ed
Rahmat Agung Julians <rahmatagungj@Gmail.com>
2021-09-07 21:56:55 +0700

    refactoring using new syntax

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,21 @@
 // Get the modal
-var modal = document.getElementById("modalwrapper");
+const modal = document.getElementById("modalwrapper");
 
 // Get the button that opens the modal
-var btn = document.getElementById("moreTagsBtn");
+const btn = document.getElementById("moreTagsBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+const span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal
-btn.onclick = function () {
-  modal.style.display = "block";
-};
+btn.onclick = () => modal.style.display = "block";
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-};
+span.onclick = () => modal.style.display = "none";
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+window.onclick = (e) => {
+  if (e.target == modal) modal.style.display = "none";
 };
 
 // =====================
@@ -44,6 +38,8 @@ function setDarkMode() {
 // check localstorage
 if (localStorage.getItem("preferredTheme") == "dark") setDarkMode();
 
-document.getElementById("darkModeBtn").addEventListener("click", function () {
-  setDarkMode();
-});
+// Get dark mode button
+const darkModeBtn = document.getElementById("darkModeBtn")
+
+// Add event to dark mode button
+darkModeBtn.onclick = () => setDarkMode();
